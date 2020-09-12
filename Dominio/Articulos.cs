c6a4bbb09a137b0cfbb0fd435c5dc92db2c6bbc5
Ailen Diaz <63076335+ailendiaz@users.noreply.github.com>
2020-09-12 20:04:33 -0300

    Creacion Clase Articulos

---
+++ Dominio/Articulos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulos
+    {
+        private int ID;
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string URLima { get; set; }
+
+    }
+}
