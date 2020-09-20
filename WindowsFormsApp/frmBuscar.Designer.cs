86d567a28f7bda01b67dc92aaf8f9604490a157c
Ailen Diaz <63076335+ailendiaz@users.noreply.github.com>
2020-09-20 19:26:10 -0300

    Agrego los ultimos detalles de visualizacion.

--- WindowsFormsApp/frmBuscar.Designer.cs
+++ WindowsFormsApp/frmBuscar.Designer.cs
@@ -34,23 +34,31 @@
             this.lblBuscar = new System.Windows.Forms.Label();
             this.btmodificar = new System.Windows.Forms.Button();
             this.bteliminar = new System.Windows.Forms.Button();
+            this.panelbuscar = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBuscar)).BeginInit();
+            this.panelbuscar.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(76, 180);
+            this.btnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnBuscar.FlatAppearance.BorderSize = 0;
+            this.btnBuscar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnBuscar.Location = new System.Drawing.Point(4, 143);
             this.btnBuscar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(112, 35);
+            this.btnBuscar.Size = new System.Drawing.Size(231, 35);
             this.btnBuscar.TabIndex = 0;
             this.btnBuscar.Text = "Buscar";
-            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.UseVisualStyleBackColor = false;
             this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // txtBuscar
             // 
-            this.txtBuscar.Location = new System.Drawing.Point(48, 123);
+            this.txtBuscar.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtBuscar.Location = new System.Drawing.Point(42, 66);
             this.txtBuscar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtBuscar.Multiline = true;
             this.txtBuscar.Name = "txtBuscar";
@@ -63,60 +71,83 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvBuscar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvBuscar.Location = new System.Drawing.Point(285, 98);
+            this.dgvBuscar.Location = new System.Drawing.Point(306, 73);
             this.dgvBuscar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvBuscar.Name = "dgvBuscar";
             this.dgvBuscar.RowHeadersWidth = 62;
-            this.dgvBuscar.Size = new System.Drawing.Size(682, 258);
+            this.dgvBuscar.Size = new System.Drawing.Size(663, 258);
             this.dgvBuscar.TabIndex = 2;
             // 
             // lblBuscar
             // 
             this.lblBuscar.AutoSize = true;
-            this.lblBuscar.Location = new System.Drawing.Point(26, 98);
+            this.lblBuscar.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBuscar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblBuscar.Location = new System.Drawing.Point(59, 34);
             this.lblBuscar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblBuscar.Name = "lblBuscar";
-            this.lblBuscar.Size = new System.Drawing.Size(230, 20);
+            this.lblBuscar.Size = new System.Drawing.Size(135, 18);
             this.lblBuscar.TabIndex = 3;
-            this.lblBuscar.Text = "Ingrese el Codigo del Producto:";
+            this.lblBuscar.Text = "Ingrese el Codigo:";
             // 
             // btmodificar
             // 
-            this.btmodificar.Location = new System.Drawing.Point(76, 266);
+            this.btmodificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btmodificar.FlatAppearance.BorderSize = 0;
+            this.btmodificar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.btmodificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btmodificar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btmodificar.Location = new System.Drawing.Point(0, 223);
             this.btmodificar.Name = "btmodificar";
-            this.btmodificar.Size = new System.Drawing.Size(112, 32);
+            this.btmodificar.Size = new System.Drawing.Size(235, 32);
             this.btmodificar.TabIndex = 4;
             this.btmodificar.Text = "Modificar";
-            this.btmodificar.UseVisualStyleBackColor = true;
+            this.btmodificar.UseVisualStyleBackColor = false;
             this.btmodificar.Click += new System.EventHandler(this.btmodificar_Click);
             // 
             // bteliminar
             // 
-            this.bteliminar.Location = new System.Drawing.Point(76, 352);
+            this.bteliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.bteliminar.FlatAppearance.BorderSize = 0;
+            this.bteliminar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.bteliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bteliminar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.bteliminar.Location = new System.Drawing.Point(0, 294);
             this.bteliminar.Name = "bteliminar";
-            this.bteliminar.Size = new System.Drawing.Size(112, 35);
+            this.bteliminar.Size = new System.Drawing.Size(235, 35);
             this.bteliminar.TabIndex = 6;
             this.bteliminar.Text = "Eliminar";
-            this.bteliminar.UseVisualStyleBackColor = true;
+            this.bteliminar.UseVisualStyleBackColor = false;
             this.bteliminar.Click += new System.EventHandler(this.bteliminar_Click);
+            // 
+            // panelbuscar
+            // 
+            this.panelbuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.panelbuscar.Controls.Add(this.lblBuscar);
+            this.panelbuscar.Controls.Add(this.bteliminar);
+            this.panelbuscar.Controls.Add(this.txtBuscar);
+            this.panelbuscar.Controls.Add(this.btmodificar);
+            this.panelbuscar.Controls.Add(this.btnBuscar);
+            this.panelbuscar.Location = new System.Drawing.Point(-1, 2);
+            this.panelbuscar.Name = "panelbuscar";
+            this.panelbuscar.Size = new System.Drawing.Size(235, 443);
+            this.panelbuscar.TabIndex = 7;
             // 
             // frmBuscar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
             this.ClientSize = new System.Drawing.Size(1023, 446);
-            this.Controls.Add(this.bteliminar);
-            this.Controls.Add(this.btmodificar);
-            this.Controls.Add(this.lblBuscar);
+            this.Controls.Add(this.panelbuscar);
             this.Controls.Add(this.dgvBuscar);
-            this.Controls.Add(this.txtBuscar);
-            this.Controls.Add(this.btnBuscar);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmBuscar";
             this.Text = "Buscar Articulo";
             ((System.ComponentModel.ISupportInitialize)(this.dgvBuscar)).EndInit();
+            this.panelbuscar.ResumeLayout(false);
+            this.panelbuscar.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -128,5 +159,6 @@
         private System.Windows.Forms.Label lblBuscar;
         private System.Windows.Forms.Button btmodificar;
         private System.Windows.Forms.Button bteliminar;
+        private System.Windows.Forms.Panel panelbuscar;
     }
 }
--- WindowsFormsApp/frmListadoDeArticulos.Designer.cs
+++ WindowsFormsApp/frmListadoDeArticulos.Designer.cs
@@ -37,34 +37,33 @@
             // DGVLista
             // 
             this.DGVLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGVLista.Location = new System.Drawing.Point(11, 21);
-            this.DGVLista.Margin = new System.Windows.Forms.Padding(2);
+            this.DGVLista.Location = new System.Drawing.Point(24, 72);
             this.DGVLista.Name = "DGVLista";
             this.DGVLista.RowHeadersWidth = 62;
             this.DGVLista.RowTemplate.Height = 28;
-            this.DGVLista.Size = new System.Drawing.Size(471, 240);
+            this.DGVLista.Size = new System.Drawing.Size(706, 369);
             this.DGVLista.TabIndex = 0;
             this.DGVLista.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVLista_CellContentDoubleClick);
             this.DGVLista.SelectionChanged += new System.EventHandler(this.DGVLista_SelectionChanged);
             // 
             // pbarticulo
             // 
-            this.pbarticulo.Location = new System.Drawing.Point(518, 21);
-            this.pbarticulo.Margin = new System.Windows.Forms.Padding(2);
+            this.pbarticulo.Location = new System.Drawing.Point(759, 72);
             this.pbarticulo.Name = "pbarticulo";
-            this.pbarticulo.Size = new System.Drawing.Size(303, 240);
+            this.pbarticulo.Size = new System.Drawing.Size(454, 369);
             this.pbarticulo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbarticulo.TabIndex = 1;
             this.pbarticulo.TabStop = false;
             // 
             // frmListado
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(843, 344);
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(1264, 529);
             this.Controls.Add(this.pbarticulo);
             this.Controls.Add(this.DGVLista);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "frmListado";
             this.Text = "Listado de Articulos";
             this.Load += new System.EventHandler(this.Form1_Load);
